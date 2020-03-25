9284a9c1ff65d1852b64e1c2efec7d1af58a8442
jeffmer <jeffmer@users.noreply.github.com>
2020-03-25 10:56:31 +0000

    Add files via upload

---
+++ apps/bigclock/bigclock.js
@@ -0,0 +1,34 @@
+
+
+function drawTime() {
+  var d = new Date();
+  var da = d.toString().split(" ");
+  var time = da[4].substr(0, 5).split(":");
+  var hours = time[0],
+    minutes = time[1];
+  g.setColor(1,1,1);
+  g.setFont("Vector",100);
+  g.drawString(hours,50,10,true);
+  g.drawString(minutes,50,130,true);
+}
+
+Bangle.on('lcdPower',function(on) {
+  if (on) {
+    g.clear();
+    Bangle.drawWidgets();
+    drawTime();
+  }
+});
+
+Bangle.setLCDBrightness(1);
+g.clear();
+drawTime();
+// Show launcher when middle button pressed
+setWatch(Bangle.showLauncher, BTN2, {repeat:false,edge:"falling"});
+// change watch
+setWatch(function(){
+    load("anaclock.app.js");
+}, BTN4, {repeat:false,edge:"rising"});
+setWatch(function(){
+    load("digiclock.app.js");
+}, BTN5, {repeat:false,edge:"rising"});
