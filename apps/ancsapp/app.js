ee751a557be458d18653cc46a4ad77624b20108c
jeffmer <jeffmer@users.noreply.github.com>
2020-04-27 10:23:14 +0100

    update

--- apps/ancsapp/app.js
+++ apps/ancsapp/app.js
@@ -1,7 +1,6 @@
 Serial1.setConsole(true);
 console.log("Starting ANCS test prog");
 
-
 function advert(){
 console.log("Starting Advertising");
 NRF.setAdvertising([
@@ -16,10 +15,19 @@ NRF.setAdvertising([
     0xCE,0xB5,
     0x31,0xF4,0x05,0x79],{});
 }
-  
-  var gatt;
 
-  NRF.on('connect',function(addr){
+var state = {
+    gatt:null,
+    ancs:null,
+    ignore:true,
+    notuid:0,
+    com:new Uint8Array([0,0,0,0,0,1,20,0,3,40,0]),
+    buf:new Uint8Array(72),
+    inp:0
+};   
+
+NRF.on('connect',function(addr){
+    var gatt;
     console.log("connect from ",addr);
     NRF.connect(addr).then(function(g) {
     gatt = g;
@@ -33,29 +41,21 @@ NRF.setAdvertising([
           var sec = gatt.getSecurityStatus();
           console.log(sec);
           if (!sec.connected) {clearInterval(ival); return;}
-          if (!sec.connected) {clearInterval(ival); return;}
-          if (sec.connected && sec.encrypted){clearInterval(ival);  do_ancs(); return;}
+          if (sec.connected && sec.encrypted){
+            clearInterval(ival);  
+            state.gatt=gatt;
+            do_ancs(); 
+            return;
+          }
       },2000);
     }).catch(function(e){
       console.log("ERROR",e);
     });
-  });
-
-const category = ["Other","Call ","Missd","Vmail","Msg  ","Sched","Email","News ","Fitn ","Busn ","Locn ","Entn "];
-const eventID  = ["New ","Mod ","Old "];
-
-var ancs = {primary:null, notify:null, control:null, data:null};
-
-var notification;
-
-function print_notify(v){
-  notification = v;
-  Serial1.print(eventID[v[0]]+category[v[2]]);
-  Serial1.println(" Count "+v[3]+" Uid "+v[4]);
- }
+});
 
 function do_ancs() {
-  gatt.getPrimaryService("7905F431-B5CE-4E99-A40F-4B1E122D00D0").then(function(s) {
+  var ancs = {primary:null, notify:null, control:null, data:null};
+  state.gatt.getPrimaryService("7905F431-B5CE-4E99-A40F-4B1E122D00D0").then(function(s) {
     ancs.primary=s;
     return s.getCharacteristic("9FBF120D-6301-42D9-8C58-25E699A21DBD");
   }).then(function(c) {
@@ -66,68 +66,50 @@ function do_ancs() {
     return ancs.primary.getCharacteristic("22EAC6E9-24D6-4BB5-BE44-B36ACE7C7BFB");
   }).then(function(c) {
     ancs.data =c;
+    state.ancs=ancs;
     console.log("Got services");
+    ancs.notify.on('characteristicvaluechanged', function(ev) {
+      getnotify(ev.target.value);
+    });
+    ancs.data.on('characteristicvaluechanged', function(e) {
+      console.log(e.target.value);
+      var i = state.inp;
+      if (i>=52) return;
+      state.buf.set(e.target.value.buffer,i);
+      state.inp+=e.target.value.buffer.length;
+    });
+    state.ignore=true;
+    ancs.notify.startNotifications().then(function(){
+      setTimeout(function(){
+         state.ignore=false;
+         console.log("Waiting Notifications");
+         ancs.data.startNotifications().then(function(){
+            console.log("ready for command");
+         });  
+      },5000);
+    });
   }).catch(function() {
      console.log("Something's broken.");
   });
 }
 
-function start(){
-  ancs.notify.on('characteristicvaluechanged', function(event) {
-    print_notify(event.target.value.buffer);
+/*
+const category = ["Other","Call ","Missd","Vmail","Msg  ","Sched","Email","News ","Fitn ","Busn ","Locn ","Entn "];
+*/
+
+function getnotify(d){
+  if (state.ignore) return;
+  var eid = d.getUint8(0);
+  if (eid!=0) return;
+  var cat = d.getUint8(2);
+  if(cat!=2) return; 
+  state.notuid = d.getUint32(4,true);
+  var v = DataView(state.com.buffer);
+  v.setUint32(1,state.notuid,true);
+  state.ancs.control.writeValue(state.com).then(function(){
+    setTimeout(function(){
+       console.log(state.buf);
+       //state.inp=0;
+    }, 2000);
   });
-  ancs.notify.startNotifications().then(function(){
-     console.log("Waiting Notifications");
-  });
-}  
-
-function cmd1(id,atr){
-  var v = notification;
-  var com = new Uint8Array(7);
-  com[0]=0;
-  com[1]=id;
-  com[5]=0;
-  com[6]=atr;
-  return ancs.control.writeValue(com);
-}
-function cmd2(id,atr){
-  var v = notification;
-  var com = new Uint8Array(9);
-  com[0]=0;
-  com[1]=id;
-  com[5]=0;
-  com[6]=atr;
-  com[7]=64;
-  return ancs.control.writeValue(com);
-}
-
-function cmd5(id,atr){
-  var v = notification;
-  var com = new Uint8Array(12);
-  com[0]=0;
-  com[1]=id;
-  com[5]=0;
-  com[6]=1;
-  com[7]=16;
-  com[9]=3;
-  com[10]=32;
-  return ancs.control.writeValue(com);
-}
-
-function print_data(b){
-  var txt = "";
-  for (var i=0; i<b.length; ++i){
-    txt+=String.fromCharCode(b[i]);
-  }
- console.log(txt); 
-}
-
-function getdetails(){
-  ancs.data.on('characteristicvaluechanged', function(event) {
-    console.log(event.target.value);
-    print_data(event.target.value.buffer);
-  });
-  ancs.data.startNotifications().then(function(){
-     console.log("ready for command");
-  });  
 }
