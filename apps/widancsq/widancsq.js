e010985f7e9827aface81427865a8deb6fa76316
jeffmer <jeffmer@users.noreply.github.com>
2020-05-18 10:50:23 +0100

    Update widancsq.js

--- apps/widancsq/widancsq.js
+++ apps/widancsq/widancsq.js
@@ -175,7 +175,7 @@
         Bangle.buzz(500).then(()=>{buzzing=false;});
     }
     if (state.current.cat!=1){
-      E.showAlert(message,title).then(release_screen.bind(null,true));
+      E.showAlert(message,title).then(release_screen.bind(null,false));
     } else {
       E.showPrompt(message,{title:title,buttons:{"Accept":true,"Cancel":false}}).then(send_action);
     }
