21daaae336052e22b782fc6ec8348e76de4da483
jeffmer <jeffmer@users.noreply.github.com>
2020-05-21 11:16:40 +0100

    Update digiclock.js

--- apps/digiclock/digiclock.js
+++ apps/digiclock/digiclock.js
@@ -2,7 +2,7 @@
 var buf = Graphics.createArrayBuffer(240,92,1,{msb:true});
 function flip() {
   g.setColor(1,1,1);
-  g.drawImage({width:buf.getWidth(),height:buf.getHeight(),buffer:buf.buffer},0,77);
+  g.drawImage({width:buf.getWidth(),height:buf.getHeight(),buffer:buf.buffer},0,85);
 }
 
 /* Draw a transition between lastText and thisText.
@@ -16,7 +16,7 @@ function drawTime() {
   buf.setFont("Vector",42);
   buf.setFontAlign(0,-1);
   buf.drawString(time,buf.getWidth()/2,0);
-  buf.setFont("Vector",18);
+  buf.setFont("6x8",2);
   buf.setFontAlign(0,-1);
   var date = d.toString().substr(0,15);
   buf.drawString(date, buf.getWidth()/2, 70);
