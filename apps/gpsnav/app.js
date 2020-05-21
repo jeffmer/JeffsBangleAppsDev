25307b1faf620e9d2246d42d0223f8df8a8cbb07
jeffmer <jeffmer@users.noreply.github.com>
2020-05-21 11:33:46 +0100

    Update app.js

--- apps/gpsnav/app.js
+++ apps/gpsnav/app.js
@@ -228,7 +228,7 @@ function nextwp(inc){
 }
 
 function doselect(){
-  if (selected && waypoints[wpindex].lat===undefined && savedfix.fix) {
+  if (selected && wpindex!=0 && waypoints[wpindex].lat===undefined && savedfix.fix) {
      waypoints[wpindex] ={name:"@"+wp.name, lat:savedfix.lat, lon:savedfix.lon};
      wp = waypoints[wpindex];
      require("Storage").writeJSON("waypoints.json", waypoints);
